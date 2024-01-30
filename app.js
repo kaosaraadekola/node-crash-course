604171932fdf386aed9268af9ad48ae18b1ea321
Kaosara Adekola <kaosaraidris@hotmail.co.uk>
2024-01-30 16:02:32 +0000

    movies data all showing

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require ("express")
 
+//this connection is for the omdb task
+const { makeDBConnectionPool } = require ("./dbhelp")
+const pool = makeDBConnectionPool ("omdb")
+
 //express app
 const app = express();
 
@@ -13,21 +17,6 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.get('/objectofquotes', (req, res) => {
-    const quotes = [
-        { title: "Quote One", snippet: "Believe you can, and you're halfway there." },
-        { title: "Quote Two", snippet: "The only way to do great work is to love what you do." },
-        { title: "Quote Three", snippet: "The future belongs to those who believe in the beauty of their dreams." },
-    ];
-    res.render('objectofquotes', { quotes : quotes });
-});
-
-
-app.get('/arrayofteams', (req, res) => {
-    const teams = ["Manchester United", "Manchester City", "Chelsea", "Liverpool"];
-    res.render('arrayofteams', {teams});
-});
-
 
 app.get("/about", (req, res) => {
     res.render("about")
@@ -38,12 +27,35 @@ app.get("/blogs/create", (req, res) => {
     res.render("create")
 });
 
+
+
+//getting movies Sam has been in from the movie omdb
+app.get('/movies', async (req, res) => {
+    const dbResult = await pool.query ("select movie_name from casts_view where person_name = 'Sam Worthington'");
+    res.render('movies', {movies: dbResult.rows});
+});
+
+
+
+// 
+app.get('/objectofquotes', (req, res) => {
+    const quotes = [
+        { title: "Quote One", snippet: "Believe you can, and you're halfway there." },
+        { title: "Quote Two", snippet: "The only way to do great work is to love what you do." },
+        { title: "Quote Three", snippet: "The future belongs to those who believe in the beauty of their dreams." },
+    ];
+    res.render('objectofquotes', { quotes : quotes });
+});
+
+
+//
+app.get('/arrayofteams', (req, res) => {
+    const teams = ["Manchester United", "Manchester City", "Chelsea", "Liverpool"];
+    res.render('arrayofteams', {teams});
+});
+
+
 //if user types a random url this takes them to error page
 app.use((req, res) => {
         res.status(404).render("404")
     });
-
-
-// app.get("/page2", (req, res) => {
-//     res.send ("<p> Page Two </p>");
-// });
